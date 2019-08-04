df38c70b1e434e8707d1a00b039839cd0a323f75
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-08-04 16:13:48 -0500

    add examples of useEffect

--- intro-mern-frontend/src/Products/components/ListProducts.js
+++ intro-mern-frontend/src/Products/components/ListProducts.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 
 const ListProducts = () => {
   const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timeId = setInterval(() => {
+      console.log('useEffect!')
+      setIsLoading(!isLoading)
+    }, 2000)
+
+    return () => clearInterval(timeId)
+  })
+
+  useEffect(() => {
+    console.log('only once time')
+  }, [isLoading])
 
   return (
     isLoading
